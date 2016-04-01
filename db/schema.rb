5faa42ba6da4216901040a19de4998e0c21fa1c4
Dextostone <vickykumar.3291@gmail.com>
2016-04-01 16:19:53 +0530

    WIP Getting started

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160317111506) do
+ActiveRecord::Schema.define(version: 20160401071318) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.text     "text",       limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "statuses", force: :cascade do |t|
     t.string   "name",       limit: 255
